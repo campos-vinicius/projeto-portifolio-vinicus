f727147bfb246ab6f645ad90f0ea3691842df035
campos-vinicius <camposvinicius772@gmail.com>
2026-06-05 21:33:05 -0300

    upload de implementações
    foram adicionadas novas funcionalidades com javascript e mediaQuery para celulares.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const button = document.querySelector(".pageUp");
 
+//  dependendo de onde o usuário estiver o botão pageUp é ocultado ou exibido
+
 window.addEventListener("scroll", () => {
   if (window.scrollY > 300) {
     button.style.display = "flex";
@@ -34,6 +36,8 @@ window.addEventListener("scroll", () => {
     }
   });
 
+  //  dependendo da localização do usuário na tela o currentSection é alterado e consequentemente a
+  //  classe ativo que realça o sidebar é substítuido
   if (
     window.scrollY + window.innerHeight >=
     document.documentElement.scrollHeight - 10
@@ -49,3 +53,30 @@ window.addEventListener("scroll", () => {
   });
 });
 
+// formulários
+
+document.querySelector("#form").addEventListener("submit", (event) => {
+  const inputEmail = document.querySelector("#email");
+  const inputName = document.querySelector("#name");
+
+  let contentName = inputName.value;
+  let contentEmail = inputEmail.value;
+
+  if (!contentEmail.includes("@")) {
+    alert("insira um email válido, por gentileza");
+    event.preventDefault();
+  } else if (/\d/.test(contentName)) {
+    alert("insira um nome válido, por gentileza");
+    event.preventDefault();
+  } else {
+    alert("formulário enviado com sucesso"); //o formulário nunca será enviado, até que o usuário preencha corretamente os seus dados
+  }
+});
+
+//  toda vez que o usuário seleciona uma das opções do select mobile,
+//  ele será direcionado a seção que deseja
+
+document.getElementById("navegacao").addEventListener("change", function () {
+  const target = this.value;
+  window.location.hash = target;
+});
